be8ab87a9651da19559a9ba49b9a8ce8563f15a3
Sharmaine <sharmainetaylor@ymail.com>
2022-03-12 00:25:33 +0000

    No changes made by me

--- server/server.js
+++ server/server.js
@@ -7,12 +7,17 @@ const port = parseInt(process.env.PORT || "3000");
 
 const server = http.createServer(app);
 
+// app.listen(3100, function () {
+// 	console.log("Server is listening on port 3000. Ready to accept requests!");
+// });
+
 server.on("listening", () => {
 	const addr = server.address();
 	const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
 	// eslint-disable-next-line no-console
 	console.log(`Listening on ${bind}`);
 });
+
 
 process.on("SIGTERM", () => server.close(() => disconnectDb()));
 
