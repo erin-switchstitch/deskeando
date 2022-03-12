4ddfd4cd9a258568a0a937dc5b4599a7c2f486fe
Sharmaine <sharmainetaylor@ymail.com>
2022-03-12 00:29:19 +0000

    Seven end points created to test different ways to pull out desk booking data from the json file

--- server/api.js
+++ server/api.js
@@ -1,9 +1,42 @@
 import { Router } from "express";
+import desks from "./tem-templateData.json";
 
 const router = Router();
 
 router.get("/", (_, res) => {
-	res.json({ message: "Hello, world!" });
+	res.send(desks);
 });
+
+router.get("/wheelchair", (_, res) => {
+	const desksWheelchairUsers = desks.filter((desk) => desk.desk_features.includes("wheelchair"));
+	res.send(desksWheelchairUsers);
+});
+
+router.get("/toilet", (_, res) => {
+	const desksNearToilet = desks.filter((desk) => desk.desk_features.includes("toilet"));
+	res.send(desksNearToilet);
+});
+
+router.get("/true", (_, res) => {
+	const deskBookedTrue = desks.filter((desk) => desk.desk_booked === true);
+	res.send(deskBookedTrue);
+});
+
+router.get("/false", (_, res) => {
+	const deskBookedTrue = desks.filter((desk) => desk.desk_booked === false);
+	res.send(deskBookedTrue);
+});
+
+router.get("/moreFeatures", (_, res) => {
+	const moreFeatures = desks.filter((desk) => desk.desk_features.includes("wheelchair") && desk.desk_features.includes("screen reader"));
+	res.send(moreFeatures);
+});
+
+router.get("/desks/:desk_id", (req, res) => {
+	const deskId = +req.params.desk_id;
+	const requireDeskId = desks.filter((desk) => desk.desk_id === deskId);
+	res.send(requireDeskId);
+});
+
 
 export default router;
