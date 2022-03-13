1eef981fb2065b2d5b24dd46fa5a95cb59189271
Sharmaine <sharmainetaylor@ymail.com>
2022-03-13 17:25:32 +0000

    Sharmaine/Amanda/Bimbola/fixes#12: We created end points to pull in all data from the bookings and desks database

--- server/api.js
+++ server/api.js
@@ -1,42 +1,76 @@
 import { Router } from "express";
-import desks from "./tem-templateData.json";
+// import desks from "./tem-templateData.json";
 
 const router = Router();
 
-router.get("/", (_, res) => {
-	res.send(desks);
+const { Pool } = require("pg");
+
+const pool = new Pool({
+	user: "sharmainetaylor",
+	host: "localhost",
+	database: "deskeando",
+	password: "",
+	port: 5432,
 });
 
-router.get("/wheelchair", (_, res) => {
-	const desksWheelchairUsers = desks.filter((desk) => desk.desk_features.includes("wheelchair"));
-	res.send(desksWheelchairUsers);
+
+//Get all desks;
+router.get("/desks", (req, res) => {
+	pool
+		.query("SELECT * FROM desks")
+		.then((result) => {
+			res.json(result.rows);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json(error);
+		});
 });
 
-router.get("/toilet", (_, res) => {
-	const desksNearToilet = desks.filter((desk) => desk.desk_features.includes("toilet"));
-	res.send(desksNearToilet);
+//Get all bookings;
+router.get("/bookings", (req, res) => {
+	pool
+		.query("SELECT * FROM bookings")
+		.then((result) => {
+			res.json(result.rows);
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json(error);
+		});
 });
 
-router.get("/true", (_, res) => {
-	const deskBookedTrue = desks.filter((desk) => desk.desk_booked === true);
-	res.send(deskBookedTrue);
-});
 
-router.get("/false", (_, res) => {
-	const deskBookedTrue = desks.filter((desk) => desk.desk_booked === false);
-	res.send(deskBookedTrue);
-});
 
-router.get("/moreFeatures", (_, res) => {
-	const moreFeatures = desks.filter((desk) => desk.desk_features.includes("wheelchair") && desk.desk_features.includes("screen reader"));
-	res.send(moreFeatures);
-});
+// router.get("/wheelchair", (_, res) => {
+// 	const desksWheelchairUsers = desks.filter((desk) => desk.desk_features.includes("wheelchair"));
+// 	res.send(desksWheelchairUsers);
+// });
 
-router.get("/desks/:desk_id", (req, res) => {
-	const deskId = +req.params.desk_id;
-	const requireDeskId = desks.filter((desk) => desk.desk_id === deskId);
-	res.send(requireDeskId);
-});
+// router.get("/toilet", (_, res) => {
+// 	const desksNearToilet = desks.filter((desk) => desk.desk_features.includes("toilet"));
+// 	res.send(desksNearToilet);
+// });
 
+// router.get("/true", (_, res) => {
+// 	const deskBookedTrue = desks.filter((desk) => desk.desk_booked === true);
+// 	res.send(deskBookedTrue);
+// });
+
+// router.get("/false", (_, res) => {
+// 	const deskBookedTrue = desks.filter((desk) => desk.desk_booked === false);
+// 	res.send(deskBookedTrue);
+// });
+
+// router.get("/moreFeatures", (_, res) => {
+// 	const moreFeatures = desks.filter((desk) => desk.desk_features.includes("wheelchair") && desk.desk_features.includes("screen reader"));
+// 	res.send(moreFeatures);
+// });
+
+// router.get("/desks/:desk_id", (req, res) => {
+// 	const deskId = +req.params.desk_id;
+// 	const requireDeskId = desks.filter((desk) => desk.desk_id === deskId);
+// 	res.send(requireDeskId);
+// });
 
 export default router;
