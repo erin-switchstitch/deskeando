1948f333bff092b17fab5c98ecaac6d5079c87f4
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2022-03-21 18:58:28 +0000

    Setting up appropriate routing for the signInUp page

---
+++ client/src/components/SignInUp.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "../stylings/SignInUp.css";
+
+
+function SignInUp(){
+
+
+    return(
+        <div>
+            {/* Add the form for login here */}
+            <div  className="SignIn">
+                <h1>Sign In</h1>
+            </div>
+            {/* Add the form for registration here */}
+            <div className="signUP">
+                <h1>Sign Up</h1>
+            </div>
+        </div>
+    );
+}
+
+export default SignInUp;
