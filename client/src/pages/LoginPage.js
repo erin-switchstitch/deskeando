da73519c894f224f574de42afc0145a4b12f362a
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2022-03-24 07:35:55 +0000

    Made a toggle button to allow users switch between the Sign In and Sign Up components

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import SignIn from "../components/SignIn";
 import SignUp from "../components/SignUp";
 import "../stylings/SignUp.css";
 
 export default function LoginPage(props){
+    const [login, setLogin] = useState(true);
 
     //  ↓↓↓↓↓ globalUserDetails useState AND setGlobalUserDetails setState ↓↓↓↓↓
     let globalUserDetails = props.globalUserDetails;
@@ -11,11 +12,22 @@ export default function LoginPage(props){
     console.log(globalUserDetails);
     //  ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
+    function loginInSingUpSwitch(){
+        setLogin(!login);
+        console.log(login);
+    }
+
     return(
         <div>
-            <SignIn globalUserDetails={globalUserDetails} setGlobalUserDetails={(data)=>setGlobalUserDetails(data)}/>
-            <SignUp globalUserDetails={globalUserDetails} setGlobalUserDetails={(data)=>setGlobalUserDetails(data)}/>
+            <SignIn display={login} globalUserDetails={globalUserDetails} setGlobalUserDetails={(data)=>setGlobalUserDetails(data)} />
+            <SignUp display={login} globalUserDetails={globalUserDetails} setGlobalUserDetails={(data)=>setGlobalUserDetails(data)} />
+            <div>
+                <div> <span>{login ? "Do not have an account?" : "Already registered?"}</span>&nbsp;
+                    <button type="button" onClick={loginInSingUpSwitch}>{ login ? "Sign Up" : "Sign In"}</button>
+                </div>
+            </div>
         </div>
+
     );
 }
 
