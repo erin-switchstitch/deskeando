7d913a1c5117613594c8fee9686621da2d1e4ea8
Sharmaine <sharmainetaylor@ymail.com>
2022-03-27 17:13:35 +0100

    Created a navbar for users not signed in

---
+++ client/src/components/NavbarGuest.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../stylings/NavbarGuest.css";
+import Hero from "./Hero";
+
+
+export default function NavbarGuest() {
+	return (
+		<>
+			<div className="landingMainContainer">
+				<div className="landingLogoContainer">
+					<h1 className="landingTitle">Deskeando</h1>
+					<p className="landingText">The ultimate desk building app</p>
+				</div>
+				<nav>
+					<ul className="navUL">
+						<li className="navList">
+							<a className="navLink" href="#about">
+								About
+							</a>
+						</li>
+						<li className="navList">
+							<a className="navLink" href="#conact">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<Hero />
+		</>
+	);
+}
