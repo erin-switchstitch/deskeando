3ce2ca370c3f0058998e0abf7332fd12af28a5e4
Sharmaine <sharmainetaylor@ymail.com>
2022-03-29 20:43:43 +0100

    Links set up in navbar in order to have a navbar link changes in the nav depending on users being signed in or signed out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,28 +2,86 @@ import React, { Fragment } from "react";
 import { Link } from 'react-router-dom';
 
 const Header = (props)=> {
+    
+    //  ↓↓↓↓↓ globalUserDetails useState AND setGlobalUserDetails setState ↓↓↓↓↓
+    let globalUserDetails = props.globalUserDetails;
+    let setGlobalUserDetails = props.setGlobalUserDetails;
+    console.log(globalUserDetails);
+    //  ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+    
     return (
-        <div className="headerContainer">
-            <h1 className="headerTitle" >Deskendo</h1>
-            <h2 className="headerSubtitle"> Flexible desk booking, with Deskendo you can manage your hybrid workspace </h2>
+		<>
+			<div className="landingMainContainer">
+				<div className="landingLogoContainer">
+					<h1 className="landingTitle">Deskeando</h1>
+					<p className="landingText">The ultimate desk building app</p>
+				</div>
+				<nav>
+					<ul className="navUL">
+                        
+                    {(globalUserDetails.user_id == "") ? (
 
-            
-        
-            <Link to={'/'}>
-                <button>(login)</button>
-            </Link>
-            <Link to={'/dashboard'}>
-                <button>Dashboard</button>
-            </Link>
-            <Link to={'/booking'}>
-                <button>Make A Booking</button>
-            </Link>
-            <Link to={'/about'}>
-                <button>About</button>
-            </Link>  
+                        <>
+                            <li className="navList">
+                                <Link to={'/about'}>
+                                    <a className="navLink">About</a>
+                                </Link>  
+                            </li>
 
-        
-        </div>
+                            <li className="navList">
+                                <Link to={'/contact'}>
+                                    <a className="navLink">Contact</a>
+                                </Link>  
+                            </li>
+
+                            <li className="navList">
+                                <Link to={'/'}>
+                                    <a className="navLink">Login</a>
+                                </Link>  
+                            </li>
+
+                        </>
+
+                    ) : (
+                        <>
+                            <li className="navList">
+                                <Link to={'/dashboard'}>
+                                    <a  className="navLink">Dashboard</a>
+                                </Link>
+                            </li>
+                            <li className="navList">
+                                <Link to={'/booking'}>
+                                    <a className="navLink">Make A Booking</a>
+                                </Link>
+                            </li>
+                            <li className="navList">
+                                <Link to={'/about'}>
+                                    <a className="navLink">About</a>
+                                </Link>  
+                            </li>
+                        
+                            <li className="navList">
+                                <Link to={'/contact'}>
+                                    <a className="navLink">Contact</a>
+                                </Link>  
+                            </li>
+                        
+                            <li className="navList">
+                                <Link to={'/'}>
+                                    <a className="navLink">LogOut</a>
+                                </Link>  
+                            </li>
+                        </>
+                    )}
+
+					</ul>
+				</nav>
+			</div>
+		</>
+
+
+			
+			
     );
 };
 
