aa13973936c7f5cfa510f8cf89891bf0ff86b695
blasisi <bimbolamarialasisi@gmail.com>
2022-03-30 21:53:06 +0100

    working on aboutpage

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -14,6 +14,26 @@ export default function AboutPage(props){
         <div>
             <Header globalUserDetails={globalUserDetails} setGlobalUserDetails={(data)=>setGlobalUserDetails(data)}/>
             <h1>About Page .....</h1>
+            <p>Our Team</p>
+            <div class="social-container">
+  <h3>Github </h3>
+  <div>
+ <a href="https://github.com/blasisi"> Bimbola</a>
+ {/* <span>
+ <FontAwesomeIcon icon={["fab", "github"]} />
+
+ </span> */}
+</div>
+<div>
+ <a href="https://github.com/xtremelibasic"> Amanda</a>
+ <i class="fab fa-github-square"></i>
+</div>
+ <a href="https://github.com/Sharm-Dev"> Sharmine</a>
+ <a href="https://github.com/erin-switchstitch"> Erin</a>
+
+     </div>
+            <p></p>
+            {/* </div> */}
         </div>
     );
 }
