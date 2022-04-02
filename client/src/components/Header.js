36f6c98ccbcf61dbbe3026e3c254e428c9b11d87
erin-switchstitch <83361880+erin-switchstitch@users.noreply.github.com>
2022-04-02 06:43:18 +0100

    Fix header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,7 @@ import "../stylings/Header.css";
 import AdminSignIn from "./AdminSignIn";
 import Logo from "../images/logo6.png";
 import Hamburger from "../images/hamburger.svg";
-
 import useWindowDimensions from "./useWindowDimensions";
-
 
 
 const Header = (props)=> {
@@ -16,7 +14,6 @@ const Header = (props)=> {
     let setGlobalUserDetails = props.setGlobalUserDetails;
     console.log(globalUserDetails);
     //  ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-
 
 
 	// AMANDA !!!! 
@@ -36,52 +33,6 @@ const Header = (props)=> {
 						<ul className="navUL">
 							{globalUserDetails.user_id == "" ? (
 								<div className="navLinkWrapper">
-
-    // var Logo = require('../images/logo.png');
-    // console.log(Logo)
-
-    return (
-
-
-		// <>
-		// 	<div className="headerOuterWrapper">
-		// 		<div className="headerLogoWrapper">
-		// 			{/* <h1 className="headerTitle">Deskeando</h1> */}
-        //             <img className="headerLogo" src={Logo} alt="Deskeando Logo"/>
-		// 		</div>
-		// 		<nav className="navLinkWrapper">
-		// 			<ul className="navUL">
-
-        //             {(globalUserDetails.user_id == "") ? (
-
-        //                 <>
-        //                      <li className="navList">
-        //                         <Link to={'/about'}>
-        //                             <a className="navLink">About</a>
-        //                             {/* <AdminSignIn
-		// 					            globalUserDetails={globalUserDetails}
-		// 					            setGlobalUserDetails={(data) => setGlobalUserDetails(data)}
-		// 				            /> */}
-        //                         </Link>
-        //                     </li>
-
-        //                     <li className="navList">
-        //                         <Link to={'/contact'}>
-        //                             <a className="navLink">Contact</a>
-        //                         </Link>
-        //                     </li>
-        //             </>
-
-		// 	            <>
-				<div className="landingMainContainer">
-					<div className="landingLogoContainer">
-						<h1 className="landingTitle">Deskeando</h1>
-						<p className="landingText">The ultimate desk building app</p>
-					</div>
-					<nav className={display ? "display" : ""}>
-						<ul className="navUL">
-							{globalUserDetails.user_id == "" ? (
-								<>
 									<li className="navList">
 										<Link to={"/about"}>
 											<a className="navLink">About</a>
@@ -95,7 +46,6 @@ const Header = (props)=> {
 										</Link>
 									</li>
 									<li className="navList">
-
 										<Link to={"/"}>
 											<a className="navLink">Login</a>
 										</Link>
@@ -103,7 +53,6 @@ const Header = (props)=> {
 								</div>
 							) : (
 								<div className="navLinkWrapper">
-
 									<li className="navList">
 										<Link to={"/dashboard"}>
 											<a className="navLink">Dashboard</a>
@@ -126,7 +75,6 @@ const Header = (props)=> {
 										</Link>
 									</li>
 									<li className="navList">
-
 										<Link to={"/"}>
 											<button onClick={() => {
 												window.location.href="http://localhost:3000";
@@ -141,7 +89,6 @@ const Header = (props)=> {
 				</div>
 			// </>
 		);
-
 };
 
 export default Header;
