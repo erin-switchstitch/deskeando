6c2aa15f4131a0e53d8701b885ba6b756a0bc9b5
erin-switchstitch <83361880+erin-switchstitch@users.noreply.github.com>
2022-04-05 23:19:41 +0100

    Update common.config.js

--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -29,7 +29,7 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			favicon: "./client/src/favicon.ico",
+// 			favicon: "./client/src/favicon.ico",
 			template: "./client/src/index.html",
 		}),
 	],
