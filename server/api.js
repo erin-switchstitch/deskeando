712ce91076c33464f7688587e77693f80350f55d
erin-switchstitch <83361880+erin-switchstitch@users.noreply.github.com>
2022-04-06 00:00:44 +0100

    readded favicon 3

--- server/api.js
+++ server/api.js
@@ -22,10 +22,10 @@ if (process.env.REACT_APP_PASSWORD){
 }
 
 const pool = new Pool({
-    user:  process.env.REACT_APP_USERNAME,
-    host: "localhost",
-    database: process.env.REACT_APP_DATABASE_NAME,
-    password: process.env.REACT_APP_PASSWORD,
+    user:  "bjbpnrdubidpfr",
+    host: "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com",
+    database: "dfl0icehrl89m9",
+    password: "3a6447fbaa3b7ef3c3ee402e57ab7052dd3cd95a0f30026627b134ee4f53e5d2",
     port: 5432,
 });
 
